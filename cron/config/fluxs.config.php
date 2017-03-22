8c41cb842be513e9ef406a5ffcf95f3cce91d80b
Cédric Bazureau <cbazureau@gmail.com>
2017-03-22 08:24:25 +0100

    fix img

--- cron/config/fluxs.config.php
+++ cron/config/fluxs.config.php
@@ -4,12 +4,12 @@ $fluxs = array(
   array(
     'url' => 'https://www.smashingmagazine.com/feed/',
     'name' => 'Smashing Magazine',
-    'logo' => 'images/logos/smashingmagazine.png'
+    'logo' => '/images/logos/smashingmagazine.png'
   ),
   array(
     'url' => 'http://korben.info/feed',
     'name' => 'Korben',
-    'logo' => 'images/logos/korben.jpg'
+    'logo' => '/images/logos/korben.jpg'
 ),
   array(
     'url' => 'http://alistapart.com/main/feed',
@@ -29,32 +29,32 @@ $fluxs = array(
 array(
     'url' => 'http://www.developpez.com/index/rss',
     'name' => 'Developpez.com',
-    'logo' => 'images/logos/developpez.jpeg'
+    'logo' => '/images/logos/developpez.jpeg'
   ),
   array(
     'url' => 'https://fredcavazza.net/feed/',
     'name' => 'Fred Cavazza',
-    'logo' => 'images/logos/fredcavazza.jpg'
+    'logo' => '/images/logos/fredcavazza.jpg'
   ),
   array(
     'url' => 'https://developers.google.com/web/updates/rss.xml',
     'name' => 'Google dev',
-    'logo' => 'images/logos/chromiumchrome.png'
+    'logo' => '/images/logos/chromiumchrome.png'
   ),
   array(
     'url' => 'https://css-tricks.com/feed/',
     'name' => 'CSS Tricks',
-    'logo' => 'images/logos/csstricks.jpeg'
+    'logo' => '/images/logos/csstricks.jpeg'
   ),
   array(
     'url' => 'https://hacks.mozilla.org/feed/',
     'name' => 'Hacks Mozilla',
-    'logo' => 'images/logos/hacksmoz.png'
+    'logo' => '/images/logos/hacksmoz.png'
   ),
   array(
     'url' => 'http://ilovetypography.com/feed/',
     'name' => 'I Love Typography',
-    'logo' => 'images/logos/ilovetypography.jpg'
+    'logo' => '/images/logos/ilovetypography.jpg'
   ),
   array(
     'url' => 'http://www.quirksmode.org/blog/index.xml',
