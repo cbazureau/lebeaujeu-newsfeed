83a1af76a2cc5e8f0d35bf9fb35e1341450ef4bf
Cédric Bazureau <cbazureau@gmail.com>
2017-03-22 08:01:12 +0100

    local img

--- cron/config/fluxs.config.php
+++ cron/config/fluxs.config.php
@@ -4,12 +4,12 @@ $fluxs = array(
   array(
     'url' => 'https://www.smashingmagazine.com/feed/',
     'name' => 'Smashing Magazine',
-    'logo' => 'https://www.brandeps.com/logo-download/S/Smashing-Magazine-01.png'
+    'logo' => 'images/logos/smashingmagazine.png'
   ),
   array(
     'url' => 'http://korben.info/feed',
     'name' => 'Korben',
-    'logo' => 'https://pbs.twimg.com/profile_images/797057160366657536/vmmIoiAy.jpg'
+    'logo' => 'images/logos/korben.jpg'
 ),
   array(
     'url' => 'http://alistapart.com/main/feed',
@@ -29,32 +29,32 @@ $fluxs = array(
 array(
     'url' => 'http://www.developpez.com/index/rss',
     'name' => 'Developpez.com',
-    'logo' => 'https://pbs.twimg.com/profile_images/482433295758553088/av4cIucb_400x400.jpeg'
+    'logo' => 'images/logos/developpez.jpeg'
   ),
   array(
     'url' => 'https://fredcavazza.net/feed/',
     'name' => 'Fred Cavazza',
-    'logo' => 'https://pbs.twimg.com/profile_images/672791255265488896/9PVyUomO.jpg'
+    'logo' => 'images/logos/fredcavazza.jpg'
   ),
   array(
     'url' => 'https://developers.google.com/web/updates/rss.xml',
     'name' => 'Google dev',
-    'logo' => 'https://pbs.twimg.com/profile_images/1320180647/chromiumchrome_400x400.png'
+    'logo' => 'images/logos/chromiumchrome.png'
   ),
   array(
     'url' => 'https://css-tricks.com/feed/',
     'name' => 'CSS Tricks',
-    'logo' => 'https://pbs.twimg.com/profile_images/546011298627334146/MAeF2-yK_200x200.jpeg'
+    'logo' => 'images/logos/csstricks.jpeg'
   ),
   array(
     'url' => 'https://hacks.mozilla.org/feed/',
     'name' => 'Hacks Mozilla',
-    'logo' => 'https://pbs.twimg.com/profile_images/2631319542/aa071efb0ed133973c2ab9fea8b5f6d8_400x400.png'
+    'logo' => 'images/logos/hacksmoz.png'
   ),
   array(
     'url' => 'http://ilovetypography.com/feed/',
     'name' => 'I Love Typography',
-    'logo' => 'http://wwwhere.io/img/thumbs/iliketypography.jpg'
+    'logo' => 'images/logos/ilovetypography.jpg'
   ),
   array(
     'url' => 'http://www.quirksmode.org/blog/index.xml',
